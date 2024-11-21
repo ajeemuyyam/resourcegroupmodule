af9dd1a0f6d5063436f780ce5dc745c0e4adcf11
ajeemuyyam <ajeemuyyam@gmail.com>
2024-11-20 20:47:53 -0500

    added
    added

---
+++ resourcegroup.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "ajeerg1" {
+  name     = var.rgname
+  location = var.location
+}
---
+++ variable.tf
@@ -0,0 +1,8 @@
+variable "rgname" {
+  type = string
+  description = "resorcegroup name"
+}
+variable "location" {
+    type = string
+    description = "resource location"
+}
